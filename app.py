90ba51f280f0d4b11b0e527f0c678bb62d6c9436
yakun0113 <60337315+yakun0113@users.noreply.github.com>
2020-04-26 16:48:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def init_db():
         with app.open_resource('./schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-
+# skip the following line if this is not the first time running app.py
 init_db()
 
 def query_db(query, args=(), one=False):
